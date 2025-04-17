48d29ab2d4669d2aef4ce5b2158e8fe7c4e26329
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-04-17 11:21:00 +0300

    need end

--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List<PersonalComputer> PC = new List<PersonalComputer>();
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine($"Включить - 1\nВыключить - 0"); 
+                int Switch = int.Parse(Console.ReadLine());
+                if (Switch == 1)
+                {
+                    PC[i].On();
+                }
+                if (Switch == 2)
+                {
+                    PC[i].Off();
+                }
+                else
+                {
+                    Console.WriteLine("Неправильный ввод");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ TVSet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TVSet : ISwitchable
+    {
+        public void On()
+        {
+            Console.WriteLine("Включено");
+        }
+        public void Off()
+        {
+            Console.WriteLine("Выключено");
+        }
+    }
+}
